8a4cfa783cbcf6900b4a15690fd326fdf88eb6ef
Krish Shah <2003kshah@gmail.com>
2022-03-18 19:30:54 -0400

    deleted signin.js

--- signin.js
+++
@@ -1,8 +0,0 @@
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-        alert("You have been signed out successfully");
-        $(".data").css("display", "none");
-        $(".g-signin2").css("display", "block");
-    });
-}
